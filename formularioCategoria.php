094402fe6fa91a57bd8e72a195f102721ad29fd0
aandre100 <andre@aandre100>
2020-08-17 15:21:27 +0100

    registo categoria layout

--- formularioCategoria.php
+++ formularioCategoria.php
@@ -1,86 +1,83 @@
 <?php
 include 'menu.php';
 
- ?>
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-	<head>
-		<meta charset="utf-8">
-		<title></title>
-	</head>
-	<body>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
 
 
-		<div class="container-fluid mt--7">
-	  	<!-- Table -->
-	  	<div class="row">
-	  	  <div class="col">
-	  		<div class="card shadow">
-	  		  <div class="card-header border-0">
-	  			<h3 class="mb-0">Registo Formulário</h3>
-	  		  </div>
+	<div class="container-fluid mt--7">
+		<!-- Table -->
+		<div class="row">
+			<div class="col">
+				<div class="card shadow">
+					<div class="card-header border-0">
+						<h3 class="mb-0">Registo Formulário</h3>
+					</div>
 
-	  		  <div class="card-footer py-4">
-	  			<nav aria-label="...">
-	  			  <ul class="pagination justify-content-end mb-0">
-	  				<li class="page-item disabled">
-	  				  <a class="page-link" href="#" tabindex="-1">
-	  					<i class="fas fa-angle-left"></i>
-	  					<span class="sr-only">Previous</span>
-	  				  </a>
-	  				</li>
-	  				<li class="page-item active">
-	  				  <a class="page-link" href="#">1</a>
-	  				</li>
-	  				<li class="page-item">
-	  				  <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-	  				</li>
-	  				<li class="page-item"><a class="page-link" href="#">3</a></li>
-	  				<li class="page-item">
-	  				  <a class="page-link" href="#">
-	  					<i class="fas fa-angle-right"></i>
-	  					<span class="sr-only">Next</span>
-	  				  </a>
-	  				</li>
-	  			  </ul>
-	  			</nav>
-	  		  </div>
-	  		</div>
-	  	  </div>
-	  	</div>
-	  	
-	  	<!-- Footer -->
-	  	<footer class="footer">
-	  	  <div class="row align-items-center justify-content-xl-between">
-	  		<div class="col-xl-6">
-	  		  <div class="copyright text-center text-xl-left text-muted">
-	  			&copy; 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
-	  		  </div>
-	  		</div>
-	  		<div class="col-xl-6">
-	  		  <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-	  			<li class="nav-item">
-	  			  <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
-	  			</li>
-	  			<li class="nav-item">
-	  			  <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
-	  			</li>
-	  			<li class="nav-item">
-	  			  <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-	  			</li>
-	  			<li class="nav-item">
-	  			  <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
-	  			</li>
-	  		  </ul>
-	  		</div>
-	  	  </div>
-	  	</footer>
-	    </div>
+					<div class="container" style="margin-top: 10px">
+						<form>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<label>Nome</label>
+										<input type="text" class="form-control form-control-alternative"  placeholder="Informe uma Categoria" name="categoria" required="">
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
 
 
-		<?php
-		include 'rodape.php';
 
-		 ?>
-	</body>
+					<div class="card-footer py-4">
+						<div style="text-align: right">
+							<a href="#" role="button" class="btn btn-primary">Inscrever</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer -->
+	<div class="container">
+		<footer class="footer">
+			<div class="row align-items-center justify-content-xl-between">
+				<div class="col-xl-6">
+					<div class="copyright text-center text-xl-left text-muted">
+						&copy; 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+					</div>
+				</div>
+				<div class="col-xl-6">
+					<ul class="nav nav-footer justify-content-center justify-content-xl-end">
+						<li class="nav-item">
+							<a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+						</li>
+						<li class="nav-item">
+							<a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+						</li>
+						<li class="nav-item">
+							<a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+						</li>
+						<li class="nav-item">
+							<a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+
+	<?php
+	include 'rodape.php';
+
+	?>
+</body>
 </html>
