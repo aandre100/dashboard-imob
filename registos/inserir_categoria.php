755e25f7a55d5a696f07792da1d4d14c3b32f83c
André Cardoso <andre@spzn.pt>
2020-08-18 16:30:02 +0100

    alterei o inserir categoria

--- registos/inserir_categoria.php
+++ registos/inserir_categoria.php
@@ -1,16 +1,76 @@
 <?php
 include '../menu.php';
-require_once('../Sql.php');
+include './assets.php';
+
 $categoria = $_GET['categoria'];
-echo $categoria;
  ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 	<head>
 		<meta charset="utf-8">
-		<title></title>
+		<title>teste</title>
 	</head>
 	<body>
+    <div class="container-fluid mt--7">
+      <!-- Table -->
+      <div class="row">
+        <div class="col">
+          <div class="card shadow">
+            <div class="card-header border-1">
+              <h3 class="mb-0">Inserido com sucesso</h3>
+            </div>
+
+            <div class="container" style="margin-top: 10px">
+
+
+                <div class="card-footer py-4">
+                  <!-- rodapé -->
+                </div>
+
+            </div>
+
+
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <div class="container">
+      <footer class="footer">
+        <div class="row align-items-center justify-content-xl-between">
+          <div class="col-xl-6">
+            <div class="copyright text-center text-xl-left text-muted">
+              &copy; 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+            </div>
+          </div>
+          <div class="col-xl-6">
+            <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+              <li class="nav-item">
+                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+              </li>
+              <li class="nav-item">
+                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+
+
+    <?php
+    include 'rodape.php';
+
+    ?>
 
 	</body>
 </html>
